1152a287fbc5f6923993abce0f66d2ca6ebe5fd1
Burnout <jansilvavieira16@gmail.com>
2026-02-07 10:56:16 -0300

    garantia

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +11,11 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
 
     Animator anim;
+    Vector3 mouseWorldPosition;
+    [SerializeField] private Camera mainCamera;
+    public LayerMask DefaltLayer;
+    public Color gizmoColor = Color.green;
+    Vector2 direction;
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,13 +30,13 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         Movimento();
+       Physics2D.LinecastAll(transform.position, direction, DefaltLayer);
     }
 
     public void SetMovimento(InputAction.CallbackContext context)
     {
         movimento = context.ReadValue<Vector2>();
     }
-
     public void Movimento()
     {
         rb.AddForce(movimento * speed);
@@ -41,5 +49,24 @@ public class Player : MonoBehaviour
         float velocidadeReal = rb.linearVelocity.magnitude;
 
         anim.speed = Mathf.Lerp(anim.speed, velocidadeReal / 2, Time.deltaTime * 5f);
+    }
+    public void SetMouse(InputAction.CallbackContext value)
+    {
+        var look = value.ReadValue<Vector2>();
+        direction = Vector2.ClampMagnitude(mainCamera.ScreenToWorldPoint(look), 5f);
+        mouseWorldPosition = mainCamera.ScreenToWorldPoint(look);
+        // Vetor que transforma o ponto do espaço da tela no espaço mundial
+        mouseWorldPosition.z = 0f;
+    }
+    public void SetSensor(InputAction.CallbackContext value)
+    {
+        if (value.performed)
+        {
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawLine(transform.position, direction);
     }
 }
